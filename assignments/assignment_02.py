458dfdaac29caa4a0eac84537582c6adab1e3f79
Shubam Patial <your_github_email@example.com>
2025-11-17 21:21:15 -0500

    Done

--- assignments/assignment_02.py
+++ assignments/assignment_02.py
@@ -27,7 +27,7 @@ print(f"Net Salary After Tax: ${salary - tax_amount}")
 # Write a function that takes two integers a and b
 # and prints all even numbers between them (inclusive).
 
-def even_num_checker(start, end):
+def even_num_checker(num_1, num_2):
     start = min(num_1, num_2)
     end = max(num_1, num_2)
     print(f"Even numbers between {start} and {end} are ----")
@@ -74,25 +74,71 @@ split_number(number)
 
 # Q4
 # Write a function that returns the count of digits in a number n.
-def count(n):
-    pass
+
+def count_digits(number):
+    count = 0
+    for i in number:
+        count += 1
+    print(count)
+
+number = input("Enter a number you wanna count the total digits of = ")
+count_digits(number)
+
 # Q5
 # Write a function that returns the sum of digits of a number n.
+
+def sum_of_digits(number):
+    sum = 0
+    for i in number:
+        sum += int(i)
+    print(sum)
+number = input("Enter a number you wanna find the sum of digits of = ")
+sum_of_digits(number)
 
 # Q6
 # Write a program to print all numbers from 1 to 100
 # that are divisible by both 3 and 5.
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print(f"{i} is divisible by both 3 and 5")
 
 # Q7
 # Continuously take input number n from the user
 # and print whether it is positive or negative,
 # until the user enters "Quit".
 
+flag = True
+while flag:
+    choice = input("Enter a number or 'Quit' to exit: ").strip()
+    if choice.lower() == "quit":
+        flag = False
+        print("You can use our service anytime")
+    elif int(choice) >= 0:
+        print("The entred number is a positive number...")
+    else:
+        print("The entred number is a negative number...")
+
 # Q8
 # Create a simple calculator function:
 # calculator(a, b, operation)
 # The operation parameter can be: '+', '-', '*', '/'
 # Perform the correct arithmetic operation based on user input.
+
+num_1 = float(input("Enter your first number: "))
+operation = input("'+', '-', '*', '/' ")
+num_2 = float(input("Enter your second number: "))
+
+match operation:
+
+    case "+":
+        print(f"The sum of {num_1} and {num_2} is = {num_1 + num_2}")
+    case "-":
+        print(f"The subtraction of {num_1} and {num_2} is = {num_1 - num_2}")
+    case "*":
+        print(f"The multiply of {num_1} and {num_2} is = {num_1 * num_2}")
+    case "/":
+        print(f"The divison of {num_1} and {num_2} is = {num_1 / num_2}")
 
 # Q9
 # Write a function is_prime(n).
@@ -101,6 +147,18 @@ def count(n):
 # - Prime checking only for 2 or greater
 # - A non-prime number divides by at least one number in [2, n-1]
 
+def is_prime(n):
+    if n < 2:
+        print(f'{n} is not a prime number')
+        return
+
+    for i in range(2, n):
+        if n % i == 0:
+            print(f"{n} is  not a prime number")
+            return
+
+    print(f"{n} is a prime number")
+
 # Q10
 # Number Guessing Game:
 # Choose a secret number (hardcoded by you).
@@ -108,3 +166,16 @@ def count(n):
 # - "Too high" if guess > number
 # - "Too low" if guess < number
 # - "Correct!" if guess == number
+
+SECRET_NUMBER = 45
+
+while True:
+    guess = int(input("Enter any number from (1-100) to guess = "))
+    if guess > SECRET_NUMBER:
+        print("Too High")
+    elif guess < SECRET_NUMBER:
+        print("Too Low")
+    else:
+        print("Correct!")
+        break
+    
